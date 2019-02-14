178b57f4f21b05cfdab95ca71333a60f33a4f9dd
ArielTak <ariel@takvam.net>
2019-02-13 21:03:03 -0800

    Correcting previous upload (#58)

---
+++ TurkTurk/TurkTurk/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  TurkTurk
+//
+//  Created by Ariel Takvam on 2/13/19.
+//  Copyright © 2019 Sycorax. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+    }
+
+
+}
+
